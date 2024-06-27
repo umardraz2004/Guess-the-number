547bacb57dd4748bb13b2338f4b6abcf68959901
umardraz2004 <umardraz6965@gmail.com>
2024-06-27 23:08:35 +0500

    Some changes

--- script/script1.js
+++ script/script1.js
@@ -1,9 +1,14 @@
 const startGame = document.getElementById('startGame');
-startGame.addEventListener('click', function(e){
+startGame.addEventListener('click', function(e) {
     window.location.href = "Game.html";
 });
 
 const aboutBtn = document.getElementById('about');
 aboutBtn.addEventListener('click', event => {
     window.location.href = "about.html";
+});
+
+const back = document.getElementById('exitBtn');
+back.addEventListener('click', function(e) {
+    window.location.href = "Start.html";
 });
--- script/script2.js
+++ script/script2.js
@@ -142,3 +142,8 @@ function typeString(str, element, callback) {
         }
     }, 25);
 }
+
+const back = document.getElementById('exitBtn');
+back.addEventListener('click', function(e) {
+    window.location.href = "Start.html";
+});
